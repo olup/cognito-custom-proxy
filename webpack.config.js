539233d1de8abdc4c34c6ab8a160549bbd45bbd7
loup topalian <oup.topalian@gmail.com>
2020-09-18 08:57:35 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  entry: slsw.lib.entries,
+  externals: [nodeExternals()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+    sourceMapFilename: '[file].map'
+  },
+  optimization: {
+    // We do not want to minimize our code.
+    minimize: false
+  },
+  target: 'node',
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+    ],
+  },
+  devtool: 'source-map',
+};
